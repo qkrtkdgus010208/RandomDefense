9865e00b9d9e2d9d6fba9da5596aa8b73a1e0f51
qkrtkdgus010208 <psh010208@naver.com>
2025-08-17 00:28:14 +0900

    Init 씬 제작 및 구현

---
+++ Assets/Scripts/Init/InitScenario.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class InitScenario : MonoBehaviour
+{
+    [SerializeField]
+    private Progress progress;
+
+    private void Awake()
+    {
+        SystemSetup();
+    }
+
+    private void SystemSetup()
+    {
+        Application.targetFrameRate = 60;
+        Application.runInBackground = true;
+        Screen.SetResolution(1920, 1080, FullScreenMode.Windowed);
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+
+        // 로딩 애니메이션 시작, 재생 완료시 OnAfterProgress() 메소드 실행
+        progress.Play(OnAfterProgress);
+    }
+
+    private void OnAfterProgress()
+    {
+        GameManager.Instance.TryAutoLogin();
+    }
+
+    public void OnClickLobby()
+    {
+        Utils.LoadScene(SceneNames.Lobby);
+    }
+}
