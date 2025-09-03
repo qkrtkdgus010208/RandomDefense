7951cb8d07bb0806fcd726149ab016a53f25d188
qkrtkdgus010208 <psh010208@naver.com>
2025-09-03 17:17:43 +0900

    로그아웃 구현 (#12)

--- Assets/Scripts/Lobby/LobbyController.cs
+++ Assets/Scripts/Lobby/LobbyController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using BackEnd;
+using UnityEngine;
 
 public class LobbyController : MonoBehaviour
 {
@@ -18,5 +19,22 @@ public class LobbyController : MonoBehaviour
     public void OnClickGame()
     {
         Utils.LoadScene(SceneNames.Game);
+    }
+
+    public void OnClickLogout()
+    {
+        var bro = Backend.BMember.Logout();
+
+        if (bro.IsSuccess())
+        {
+            Debug.Log("로그아웃 성공");
+        }
+        else
+        {
+            Debug.Log("로그아웃 실패");
+        }
+
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
     }
 }
